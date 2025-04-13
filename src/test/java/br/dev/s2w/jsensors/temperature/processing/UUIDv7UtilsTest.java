f50bca85d260d597bd11fb4c1f6805fbc14feb60
WybsonSantana <wybson@hotmail.com>
2025-04-13 12:55:43 -0300

    JSensors: #01 - 09.04 - Using UUID v4 and v7 with Java

---
+++ src/test/java/br/dev/s2w/jsensors/temperature/processing/UUIDv7UtilsTest.java
@@ -0,0 +1,22 @@
+package br.dev.s2w.jsensors.temperature.processing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+class UUIDv7UtilsTest {
+
+    @Test
+    void shouldGenerateUUIDv7() {
+        UUID uuid = IdGenerator.generateTimeBasedUUID();
+
+        OffsetDateTime uuidDateTime = UUIDv7Utils.extractOffsetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+        Assertions.assertThat(uuidDateTime).isEqualTo(currentDateTime);
+    }
+
+}
