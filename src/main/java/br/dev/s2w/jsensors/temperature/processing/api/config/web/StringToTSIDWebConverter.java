3631b1a6f0163818cf9f6391d9d233d0060f2849
WybsonSantana <wybson@hotmail.com>
2025-04-20 17:55:09 -0300

    JSensors: #01 - 10.11 - Implementing Temperature Processing microservice

---
+++ src/main/java/br/dev/s2w/jsensors/temperature/processing/api/config/web/StringToTSIDWebConverter.java
@@ -0,0 +1,13 @@
+package br.dev.s2w.jsensors.temperature.processing.api.config.web;
+
+import io.hypersistence.tsid.TSID;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToTSIDWebConverter implements Converter<String, TSID> {
+
+    @Override
+    public TSID convert(String source) {
+        return TSID.from(source);
+    }
+
+}
---
+++ src/main/java/br/dev/s2w/jsensors/temperature/processing/api/model/TemperatureLogOutput.java
@@ -0,0 +1,22 @@
+package br.dev.s2w.jsensors.temperature.processing.api.model;
+
+import io.hypersistence.tsid.TSID;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+public class TemperatureLogOutput {
+
+    private UUID id;
+
+    private TSID sensorId;
+
+    private OffsetDateTime registeredAt;
+
+    private Double value;
+
+}
